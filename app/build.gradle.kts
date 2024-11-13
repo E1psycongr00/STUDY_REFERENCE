5ffa05e176d525cac9c7786a13d113f843932af4
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-11-14 08:14:47 +0900

    feat: 초기 설정

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation(libs.guava)
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass = "gradle_project.App"
+}
+
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
+}
