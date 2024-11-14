1d72f0388043d374c89f146a148aca45d1768f50
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2024-11-14 18:26:24 +0900

    build: phase 테스트를 위한 설정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@
  * The settings file is used to specify which projects to include in your build.
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/8.8/userguide/multi_project_builds.html in the Gradle documentation.
  */
+println("SETTINGS SCRIPT: This is executed during the initialization phase")
 
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
